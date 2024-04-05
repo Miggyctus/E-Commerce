15552bd5bb410722bb0a09c1ee6835c714aaa1c1
Miggyctus <mayalauptp@gmail.com>
2024-04-05 18:06:59 +0800

    first commit

---
+++ transition.js
@@ -0,0 +1,38 @@
+var slideIndex = 0;
+
+var intervalID = setInterval(nextSlide, 5000);
+
+function nextSlide() {
+  resetLoadingBar();
+  clearInterval(intervalID);
+  slideIndex++;
+  showSlide(slideIndex);
+  intervalID = setInterval(nextSlide, 5000);
+}
+
+function prevSlide() {
+  resetLoadingBar();
+  clearInterval(intervalID);
+  slideIndex--;
+  showSlide(slideIndex);
+  intervalID = setInterval(nextSlide, 5000);
+}
+
+function showSlide(index) {
+  const slides = document.querySelectorAll('.team-card');
+  if (index >= slides.length) { slideIndex = 0; }
+  else if (index < 0) { slideIndex = slides.length - 1; }
+  else {slideIndex = index; }
+  slides.forEach(slide => slide.style.transform = `translateX(-${slideIndex * 100}%)`);
+}
+
+function resetLoadingBar() {
+  // Select the loading bar element
+  var line = document.querySelector('.loading .line');
+  // Remove the line element from the DOM
+  line.parentNode.removeChild(line);
+  // Force reflow
+  void document.querySelector('.loading .line_box').offsetWidth;
+  // Re-add the line element to the DOM
+  document.querySelector('.loading .line_box').appendChild(line);
+}
