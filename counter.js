c4026ba70e05007b44cac902eb38928ecc003df2
Miggyctus <mayalauptp@gmail.com>
2024-04-08 23:09:13 +0800

    FUNCIONA EL COUNTER

---
+++ counter.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    fetch('https://miggy.pythonanywhere.com/api/increment-view', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+
+    .then(response => response.json())
+    .then(data => {
+        //Update view count on frontend
+        document.getElementById('view-count').innerText = data.viewCount;
+        console.log(data);
+    })
+    .catch(error => console.error('Error', error));
+});
